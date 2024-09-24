9d6c00c9847cb147d596833c1ba77853f533ed7d
Violet Iapalucci <violet.iapalucci@aveva.com>
2024-09-24 16:03:14 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use colored::Colorize as _;
+use std::io::Write as _;
+
+fn main() {
+    print!("\x1B[2J\x1B[1;1H");
+    println!(
+        "{}",
+        format!("┌ {}:", std::env::current_dir().unwrap().display())
+            .bold()
+            .white()
+    );
+    println!("{}", "│");
+
+    let mut dirs = Vec::new();
+    let mut files = Vec::new();
+
+    for file in std::fs::read_dir(std::env::current_dir().unwrap())
+        .unwrap()
+        .filter_map(Result::ok)
+    {
+        let path = file.path();
+        let file_name = file.file_name().into_string().unwrap();
+        if path.is_file() {
+            files.push((file_name, path));
+        } else {
+            dirs.push(file_name);
+        }
+    }
+
+    for dir in dirs {
+        let (r, g, b) = hex_to_rgb("#89b4fa").unwrap();
+        println!("│ {} {}", "".truecolor(r, g, b), dir);
+    }
+
+    for (file, path) in files {
+        let icon = devicons::icon_for_file(&devicons::File::new(&path), None);
+        let (r, g, b) = hex_to_rgb(icon.color).unwrap();
+        println!("│ {} {}", format!("{}", icon.icon).truecolor(r, g, b), file);
+    }
+
+    if std::env::consts::OS == "windows" {
+        print!("│");
+        std::io::stdout().flush().unwrap();
+    } else {
+        println!("│");
+    };
+}
+
+fn hex_to_rgb(hex: &str) -> Result<(u8, u8, u8), String> {
+    // Remove the '#' if present
+    let hex = hex.trim_start_matches('#');
+
+    // Enforce correct string length
+    if hex.len() != 6 && hex.len() != 3 {
+        return Err("Invalid hex color length".to_string());
+    }
+
+    // If the hex is 3 characters long, convert it to 6 characters
+    let hex = if hex.len() == 3 {
+        hex.chars()
+            .map(|c| format!("{c}{c}", c = c))
+            .collect::<String>()
+    } else {
+        hex.to_string()
+    };
+
+    // Parse the hex string to u8 values
+    let r = u8::from_str_radix(&hex[0..2], 16).map_err(|_| "Invalid hex value".to_string())?;
+    let g = u8::from_str_radix(&hex[2..4], 16).map_err(|_| "Invalid hex value".to_string())?;
+    let b = u8::from_str_radix(&hex[4..6], 16).map_err(|_| "Invalid hex value".to_string())?;
+
+    Ok((r, g, b))
+}
