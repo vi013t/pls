0a3c7cc46a29423cd1683c3a31baa97232489495
Violet <violet_iapalucci@outlook.com>
2024-09-25 11:33:10 -0400

    Add command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,17 @@ use colored::Colorize as _;
 use std::io::Write as _;
 
 fn main() {
+    let command_line_arguments = <CommandLineArguments as clap::Parser>::parse();
+
+    // Print current directory
     print!("\x1B[2J\x1B[1;1H");
-    println!(
-        "{}",
-        format!("┌ {}:", std::env::current_dir().unwrap().display())
-            .bold()
-            .white()
-    );
+    println!("{}", format!("┌ {}:", std::env::current_dir().unwrap().display()).bold().white());
     println!("{}", "│");
 
+    // Get files
     let mut dirs = Vec::new();
     let mut files = Vec::new();
-
-    for file in std::fs::read_dir(std::env::current_dir().unwrap())
-        .unwrap()
-        .filter_map(Result::ok)
-    {
+    for file in std::fs::read_dir(std::env::current_dir().unwrap()).unwrap().filter_map(Result::ok) {
         let path = file.path();
         let file_name = file.file_name().into_string().unwrap();
         if path.is_file() {
@@ -27,23 +22,39 @@ fn main() {
         }
     }
 
+    // Print directories
+    let (r, g, b) = hex_to_rgb(&command_line_arguments.directory_color).unwrap();
     for dir in dirs {
-        let (r, g, b) = hex_to_rgb("#89b4fa").unwrap();
-        println!("│ {} {}", "".truecolor(r, g, b), dir);
+        if command_line_arguments.show_dotfiles || !dir.starts_with(".") {
+            println!("│ {} {}", "".truecolor(r, g, b), dir);
+        }
     }
 
+    // Print files
     for (file, path) in files {
         let icon = devicons::icon_for_file(&devicons::File::new(&path), None);
         let (r, g, b) = hex_to_rgb(icon.color).unwrap();
-        println!("│ {} {}", format!("{}", icon.icon).truecolor(r, g, b), file);
+        if command_line_arguments.show_dotfiles || !file.starts_with(".") {
+            println!("│ {} {}", format!("{}", icon.icon).truecolor(r, g, b), file);
+        }
     }
 
+    // Print blank line
     if std::env::consts::OS == "windows" {
         print!("│");
         std::io::stdout().flush().unwrap();
     } else {
         println!("│");
     };
+}
+
+#[derive(clap::Parser)]
+struct CommandLineArguments {
+    #[arg(short = 'd', long)]
+    show_dotfiles: bool,
+
+    #[arg(short = 'c', long, default_value = "#89b4fa")]
+    directory_color: String,
 }
 
 fn hex_to_rgb(hex: &str) -> Result<(u8, u8, u8), String> {
@@ -57,9 +68,7 @@ fn hex_to_rgb(hex: &str) -> Result<(u8, u8, u8), String> {
 
     // If the hex is 3 characters long, convert it to 6 characters
     let hex = if hex.len() == 3 {
-        hex.chars()
-            .map(|c| format!("{c}{c}", c = c))
-            .collect::<String>()
+        hex.chars().map(|c| format!("{c}{c}", c = c)).collect::<String>()
     } else {
         hex.to_string()
     };
